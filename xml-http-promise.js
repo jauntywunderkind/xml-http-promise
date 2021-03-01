a9092208e3f17ea820beef28452a44ef9cbdd583
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2021-02-28 21:16:38 -0500

    extend then to capture response

--- xml-http-promise.js
+++ xml-http-promise.js
@@ -12,21 +12,35 @@ function makeXhpConstructor( p){
 function _ratchetReadyState( self, to){
 	if( !self.readyState> to){
 		self.readyState= to
+		return true
 	}
+	return false
 }
 
 function xhpCatch( rej){
-	_ratchetReadyState( this, 2)
+	if (_ratchetReadyState( this, 2)){
+	}
 	return this[ SYMBOL].catch( rej)
 }
 
 function xhpFinally( fn){
-	_ratchetReadyState( this, 2)
+	if( _ratchetReadyState( this, 2)){
+	}
 	return this[ SYMBOL].finally( fn)
 }
 
+
+function xhpCapture( _res){
+	this.self[ this.field]= val
+	return this.handler( val)
+}
+
 function xhpThen( res, rej){
-	_ratchetReadyState( this, 2)
+	if( _ratchetReadyState( this, 2)){
+		// if it's the first time, capture res & rej
+		res= xhpCapture.bind({ self: this, field: "response", handler: res, status: 200 })
+		rej= xhpCapture.bind({ self: this, field: "responseError", handler: rej, status: 400 })
+	}
 	return this[ SYMBOL].then( res, rej)
 }
 
