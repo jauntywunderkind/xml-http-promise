8ac333f3c767ef87794243dccc467ee6306afb65
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2021-02-28 20:44:52 -0500

    ratchet readyState

--- xml-http-promise.js
+++ xml-http-promise.js
@@ -1,14 +1,31 @@
 const SYMBOL= Symbol.for( "xml-http-promise")
 
+function xhpConstructor( ..args){
+	const self= new xhpConstructor( ...args)
+	self.readyState= 0
+	return self
+}
+
+function _ratchetReadyState( self, to){
+	if( !self.readyState){
+		self.readyState= to
+	} else if( self.readyState< to){
+		self.readyState= to
+	}
+}
+
 function xhpCatch( rej){
+	_ratchetReadyState( this, 2)
 	return this[ SYMBOL].catch( rej)
 }
 
 function xhpFinally( fn){
+	_ratchetReadyState( this, 2)
 	return this[ SYMBOL].finally( fn)
 }
 
 function xhpThen( res, rej){
+	_ratchetReadyState( this, 2)
 	return this[ SYMBOL].then( res, rej)
 }
 
